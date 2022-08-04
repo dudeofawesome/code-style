7ab5cf3aa3b1e98d0380b4485d6e43941bd4fee6
uPaymeiFixit <uPaymeiFixit@gmail.com>
2022-08-03 21:53:14 -0700

    🐛 check if prettier gem is installed
    co-authored-by: Louis Orleans <louis@orleans.io>

--- code-style/.prettierrc.js
+++ code-style/.prettierrc.js
@@ -1,7 +1,10 @@
 // https://prettier.io/docs/en/options.html
 
-module.exports = {
-  /** General */
+const exec = require('child_process').exec;
+
+const is_prettier_gem_installed = exec('gem list -i prettier') === 'true';
+
+const general = {
   singleQuote: true,
   semi: true,
   trailingComma: 'all',
@@ -16,20 +19,38 @@ module.exports = {
   // useTabs: // configured in .editorconfig->indent_size
   // tabWidth: // configured in .editorconfig->indent_style
   // endOfLine: // configured in .editorconfig->end_of_line
+};
 
-  /** HTML */
+const html = {
   htmlWhitespaceSensitivity: 'css', // prettier default
+};
 
-  /** JSX */
+const jsx = {
   jsxSingleQuote: false, // prettier default
+};
 
-  /** Vue */
+const vue = {
   vueIndentScriptAndStyle: false, // prettier default
+};
 
-  /** Ruby */
+const ruby = {
   rubyArrayLiteral: true,
   rubyHashLabel: true,
   rubyModifier: true,
   rubySingleQuote: true,
   rubyToProc: false,
 };
+
+module.exports = {
+  ...general,
+  ...html,
+  ...jsx,
+  ...vue,
+};
+
+if (is_prettier_gem_installed) {
+  module.exports = {
+    ...module.exports,
+    ...ruby,
+  };
+}
