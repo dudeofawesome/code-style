eaf1e2c7c1bbbdfe3c22e70473b92b6247623e0e
Louis Orleans <louis@orleans.io>
2023-06-06 14:52:10 -0700

    🔨 improve bump.sh usability

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,12 +1,49 @@
 #!/usr/bin/env bash
 set -e
 
-npm version --no-git-tag-version "$1"
+bump() {
+  npm version --no-git-tag-version "$1"
 
-version="$(jq --raw-output '.version' package.json)"
+  version="$(jq --raw-output '.version' package.json)"
 
-npm version "$version" --ws
+  npm version "$version" --ws
 
-git add package*.json **/package*.json
-git commit --message "🚀🔖 release v$version"
-git tag "v$version"
+  git add package*.json **/package*.json
+  git commit --message "🚀🔖 release v$version"
+  git tag "v$version"
+}
+
+help () {
+  echo -e ""
+  echo -e "Usage:\t$(basename $0) [options] <newversion>|major|minor|patch|premajor|preminor|prepatch|prerelease"
+  echo -e ""
+  echo -e "Options:"
+  echo -e "   -h, --help           Show this message"
+  echo -e ""
+  echo -e "Bumps all package versions in project to the specified version."
+}
+
+option_error () {
+  echo "Unknown argument '$1'"
+  help
+  exit 128
+}
+
+main () {
+  while getopts 'o:s:wlh-:' opt; do
+    case "$opt" in
+      h) help; exit 0;;
+      -)
+        case "${OPTARG}" in
+          help) help; exit 0;;
+          *) option_error "$OPTARG";;
+        esac
+        ;;
+      *) option_error "$OPTARG";;
+    esac
+  done
+
+  bump
+}
+
+main "$@"
