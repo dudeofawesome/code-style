a36d01a1ebf46c90c83189d6c11c732056413fe0
Louis Orleans <louis@orleans.io>
2023-09-29 17:18:30 -0700

    ✨ add jest eslint conf

---
+++ eslint-config-jest/test/index.ts
@@ -0,0 +1,31 @@
+import { describe, it } from 'node:test';
+import { ESLint } from 'eslint';
+import {
+  testRuleFail,
+  testNoFail,
+  defaultTestSet,
+} from '../../utils/testing/eslint';
+
+const linter = new ESLint({ cwd: __dirname });
+
+describe('eslint-config-node', () => {
+  defaultTestSet(linter);
+
+  describe('passes', () => {
+    it(`should have jest globals in test file`, () =>
+      testNoFail({
+        linter,
+        code: `describe('test', () => {});\n`,
+        file_path: 'test/thing.js',
+      }));
+  });
+
+  describe('fails', () => {
+    it(`should not have jest globals in non-test file`, () =>
+      testRuleFail({
+        linter,
+        code: `describe('test', () => {});\n`,
+        ruleId: 'no-undef',
+      }));
+  });
+});
