4a886561ecaa3980b9091d8418a731dfcf1284ba
Louis Orleans <louis@orleans.io>
2023-10-04 12:52:00 -0700

    🚨 fix lint

--- typescript-configs/test/index.ts
+++ typescript-configs/test/index.ts
@@ -7,27 +7,28 @@ import { readFile } from 'node:fs/promises';
 
 const exec = promisify(execCallback);
 
-describe('typescript-configs', () => {
-  describe('jest', async () => {
-    await transpile_and_test({ dir: 'jest', code_fragment: 'function foo()' });
-    await transpile_and_test({
-      dir: 'jest',
-      ts_config: 'tsconfig.test.json',
-      output_files: [
-        'index.js',
-        'index.js.map',
-        'index.d.ts',
-        'index.spec.js',
-        'index.spec.js.map',
-        'index.spec.d.ts',
-      ],
-      search_file: 'test/index.spec.js',
-      code_fragment: 'expect',
-    });
-  });
+void describe('typescript-configs', () => {
+  void describe('jest', () =>
+    Promise.all([
+      transpile_and_test({ dir: 'jest', code_fragment: 'function foo()' }),
+      transpile_and_test({
+        dir: 'jest',
+        ts_config: 'tsconfig.test.json',
+        output_files: [
+          'index.js',
+          'index.js.map',
+          'index.d.ts',
+          'index.spec.js',
+          'index.spec.js.map',
+          'index.spec.d.ts',
+        ],
+        search_file: 'test/index.spec.js',
+        code_fragment: 'expect',
+      }),
+    ]).then(() => undefined));
 
-  describe('library', async () => {
-    await transpile_and_test({
+  void describe('library', () =>
+    transpile_and_test({
       dir: 'library',
       output_files: [
         'index.js',
@@ -36,22 +37,19 @@ describe('typescript-configs', () => {
         'index.d.ts.map',
       ],
       code_fragment: `return 'bar'`,
-    });
-  });
+    }));
 
-  describe('node', async () => {
-    await transpile_and_test({
+  void describe('node', () =>
+    transpile_and_test({
       dir: 'node',
       code_fragment: 'process.env.PATH',
-    });
-  });
+    }));
 
-  describe('react', async () => {
-    await transpile_and_test({
+  void describe('react', () =>
+    transpile_and_test({
       dir: 'react',
       code_fragment: 'window.location.href',
-    });
-  });
+    }));
 });
 
 interface TranspileAndTestOpts {
@@ -67,10 +65,7 @@ async function transpile_and_test({
   output_files = ['index.js', 'index.js.map', 'index.d.ts'],
   search_file = 'index.js',
   code_fragment,
-}: TranspileAndTestOpts): Promise<{
-  stdout: string;
-  stderr: string;
-}> {
+}: TranspileAndTestOpts): Promise<void> {
   const cwd = join(__dirname, dir);
   const out_dir = join(cwd, 'dist');
   const out = await exec(
@@ -85,28 +80,26 @@ async function transpile_and_test({
     },
   );
 
-  it('should not output any errors', () => {
+  void it('should not output any errors', () => {
     strictEqual(out.stderr, '');
   });
 
-  it('should create transpiled files', () => {
+  void it('should create transpiled files', () => {
     deepEqual(
       out.stdout
         .split('\n')
         .filter(Boolean)
-        .map((line) => basename(line.split('TSFILE: ')[1])),
+        .map((line) => basename(line.split('TSFILE: ')[1] ?? '')),
       output_files,
     );
   });
 
   if (code_fragment != null) {
-    it('should contain some expected code', async () => {
+    await it('should contain some expected code', async () => {
       match(
         (await readFile(join(out_dir, search_file))).toString(),
-        new RegExp(code_fragment),
+        new RegExp(code_fragment, 'u'),
       );
     });
   }
-
-  return out;
 }
