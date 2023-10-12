91bbf53165b47e90d1ae987ff000fdab7f8a952d
Louis Orleans <louis@orleans.io>
2023-10-12 15:13:14 -0700

    🐛 fix baseUrl

--- create-configs/src/language-configuration.ts
+++ create-configs/src/language-configuration.ts
@@ -10,7 +10,7 @@ export async function create_ts_config(
   const config = {
     extends: [] as string[],
     compilerOptions: {
-      baseUrl: input_dir,
+      baseUrl: './',
       outDir: output_dir,
     },
     include: [input_dir],
