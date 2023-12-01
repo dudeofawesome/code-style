fa1d1bd84d0ce6d5fba7522b1a174e6f5b4b92a7
Louis Orleans <louis@orleans.io>
2023-12-01 09:20:12 -0800

    📝 update ESLint rule docs

--- eslint-plugin-nest/src/configs/recommended.ts
+++ eslint-plugin-nest/src/configs/recommended.ts
@@ -3,6 +3,10 @@ import { ESLint } from 'eslint';
 export const recommended: ESLint.ConfigData = {
   plugins: ['@dudeofawesome/nest'],
   rules: {
+    /**
+     * Disallow barrel files since they can lead to a number of hard to debug
+     * issues
+     */
     '@dudeofawesome/nest/no-barreling': 'error',
   },
 };
