2d73cad85c5438425776b6d1cb61d1d5be7ce244
Louis Orleans <louis@orleans.io>
2024-03-01 21:55:13 -0800

    📝 add jsdoc

--- packages/code-style/src/config-types.ts
+++ packages/code-style/src/config-types.ts
@@ -12,14 +12,24 @@ export type Builder = 'tsc' | 'esbuild' | 'swc' | 'bun' | 'babel' | 'none';
 export type Runtime = 'nodejs' | 'bun';
 
 export interface CodeStyleSetupOptions {
+  /** The general type of the project. */
   project_type: ProjectType;
+  /** Languages that will be used in the project. */
   languages: Language[];
+  /** The runtime that will be used. */
   runtime?: Runtime;
+  /** The builder that will be used. */
   builder: Builder;
+  /** The source directory. */
   input_dir?: string;
+  /** The build output directory. */
   output_dir?: string;
+  /** Any technologies, tools, etc that will be used in this project. */
   technologies: Technology[];
+  /** Whether or not this project is a library that other projects will consume. */
   library: boolean;
+  /** Whether or not to use lenient rulesets. */
   lenient: boolean;
+  /** Whether or not to overwrite files. */
   overwrite: boolean;
 }
