57965de7318525a98ddc4057bb43e78584261070
Louis Orleans <louis@orleans.io>
2024-03-02 20:48:43 -0800

    🔥 remove javascript-configs

--- packages/javascript-configs/test/node/src/index.js
+++
@@ -1,5 +0,0 @@
-import { log } from 'console';
-import { readdir } from 'node:fs/promises';
-
-log(process.env.PATH);
-readdir('./').then(log);
--- packages/javascript-configs/test/react/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function foo() {
-  alert(window.location.href);
-  return <h1>{document.body.innerHTML}</h1>;
-}
