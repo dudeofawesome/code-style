90500f1cd79c590235904bc0c6af15e960a953de
Louis Orleans <louis@orleans.io>
2024-03-19 22:48:31 -0700

    🐛 don't upgrade packages we don't need to

--- packages/create-configs/src/steps/dependencies.ts
+++ packages/create-configs/src/steps/dependencies.ts
@@ -23,9 +23,9 @@ export async function uninstall_duplicate_dependencies({
     'eslint-config-prettier',
     'eslint-plugin-prettier',
     'eslint-plugin-promise',
+    'eslint-plugin-i',
     'eslint-plugin-import',
     'eslint-plugin-json-files',
-    'eslint-plugin-i',
     'eslint-plugin-jest',
     'eslint-plugin-n',
     'eslint-plugin-node',
@@ -68,42 +68,42 @@ export async function install_dependencies({
 }: InstallDependenciesOptions) {
   const prod_packages: (string | undefined)[] = [];
   const dev_packages: (string | undefined)[] = [
-    '@code-style/code-style',
-    '@code-style/eslint-config',
-    '@code-style/eslint-npm-hoist-packages',
+    '@code-style/code-style@latest',
+    '@code-style/eslint-config@latest',
+    '@code-style/eslint-npm-hoist-packages@latest',
   ];
 
   switch (project_type) {
     case 'web-app':
       if (technologies.includes('nextjs')) {
         dev_packages.push(
-          '@code-style/eslint-config-nextjs',
-          '@code-style/eslint-npm-hoist-packages-nextjs',
+          '@code-style/eslint-config-nextjs@latest',
+          '@code-style/eslint-npm-hoist-packages-nextjs@latest',
         );
       } else if (technologies.includes('react')) {
         dev_packages.push(
-          '@code-style/eslint-config-react',
-          '@code-style/eslint-npm-hoist-packages-react',
+          '@code-style/eslint-config-react@latest',
+          '@code-style/eslint-npm-hoist-packages-react@latest',
         );
       } else {
-        dev_packages.push('@code-style/eslint-config-browser');
+        dev_packages.push('@code-style/eslint-config-browser@latest');
       }
       break;
     case 'backend':
       if (languages.includes('ts')) dev_packages.push('@types/node');
       dev_packages.push(
-        '@code-style/eslint-config-node',
-        '@code-style/eslint-npm-hoist-packages-node',
+        '@code-style/eslint-config-node@latest',
+        '@code-style/eslint-npm-hoist-packages-node@latest',
       );
       break;
     case 'cli':
       if (languages.includes('ts')) dev_packages.push('@types/node');
-      dev_packages.push('@code-style/eslint-config-cli');
+      dev_packages.push('@code-style/eslint-config-cli@latest');
       break;
   }
 
   if (languages.includes('js') || languages.includes('ts')) {
-    dev_packages.push('@code-style/typescript-configs');
+    dev_packages.push('@code-style/typescript-configs@latest');
   }
 
   for (const language of languages) {
@@ -111,20 +111,23 @@ export async function install_dependencies({
       case 'ts':
         dev_packages.push(
           'typescript',
-          '@code-style/eslint-config-typescript',
-          '@code-style/eslint-npm-hoist-packages-typescript',
+          '@code-style/eslint-config-typescript@latest',
+          '@code-style/eslint-npm-hoist-packages-typescript@latest',
         );
         break;
       case 'css':
         if (!languages.includes('scss')) {
-          dev_packages.push('stylelint', '@code-style/stylelint-config');
+          dev_packages.push(
+            'stylelint@latest',
+            '@code-style/stylelint-config@latest',
+          );
         }
         break;
       case 'scss':
         dev_packages.push(
-          'stylelint',
-          'sass-embedded',
-          '@code-style/stylelint-config-scss',
+          'stylelint@latest',
+          'sass-embedded@latest',
+          '@code-style/stylelint-config-scss@latest',
         );
         break;
       default:
@@ -137,10 +140,10 @@ export async function install_dependencies({
         dev_packages.push(
           'jest',
           '@types/jest',
-          '@code-style/eslint-config-jest',
-          '@code-style/eslint-npm-hoist-packages-jest',
+          '@code-style/eslint-config-jest@latest',
+          '@code-style/eslint-npm-hoist-packages-jest@latest',
         );
-        if (languages.includes('ts')) dev_packages.push('ts-jest');
+        if (languages.includes('ts')) dev_packages.push('ts-jest@latest');
         break;
       case 'react':
       case 'nextjs':
@@ -156,8 +159,8 @@ export async function install_dependencies({
         break;
       case 'esm':
         dev_packages.push(
-          '@code-style/eslint-config-esmodule',
-          '@code-style/eslint-npm-hoist-packages-esmodule',
+          '@code-style/eslint-config-esmodule@latest',
+          '@code-style/eslint-npm-hoist-packages-esmodule@latest',
         );
         break;
       default:
@@ -166,13 +169,13 @@ export async function install_dependencies({
 
   switch (builder) {
     case 'babel':
-      dev_packages.push('babel');
+      dev_packages.push('babel@latest');
       break;
     case 'esbuild':
-      dev_packages.push('esbuild');
+      dev_packages.push('esbuild@latest');
       break;
     case 'swc':
-      dev_packages.push('@swc/cli', '@swc/core');
+      dev_packages.push('@swc/cli@latest', '@swc/core@latest');
       break;
     case 'tsc': // we already added typescript
     case 'bun': // bun is not installed via npm
@@ -216,15 +219,9 @@ export async function install_dependencies({
   })();
 
   if (prod_packages.length > 0) {
-    await exec(
-      `${install_cmd_prod} ${prod_packages
-        .map((p) => `${p}@latest`)
-        .join(' ')}`,
-    );
+    await exec(`${install_cmd_prod} ${prod_packages.join(' ')}`);
   }
   if (dev_packages.length > 0) {
-    await exec(
-      `${install_cmd_dev} ${dev_packages.map((p) => `${p}@latest`).join(' ')}`,
-    );
+    await exec(`${install_cmd_dev} ${dev_packages.join(' ')}`);
   }
 }
