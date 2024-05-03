d33eb55b8ab84e474b7f36b31800de705dfd6634
Louis Orleans <louis@orleans.io>
2024-05-03 12:51:25 -0700

    ✨ improve coverage ignore list

--- packages/jest-configs/src/layers/coverage.ts
+++ packages/jest-configs/src/layers/coverage.ts
@@ -1,13 +1,24 @@
 import { Config } from 'jest';
 
+const ext = String.raw`[cm]?[jt]sx?`;
+
 export const coverage: Config = {
   collectCoverage: true,
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: [
-    'node_modules/',
-    'migrations/',
-    'mikro-orm\\.config\\.ts$',
-    'interfaces/',
-    'types/',
+    // dependencies
+    String.raw`/node_modules/`,
+    // migration files
+    String.raw`/migrations/`,
+    // Mikro ORM config
+    String.raw`/mikro-orm\.config\.ts$`,
+    // hidden files in the root dir (usually configs)
+    String.raw`<rootDir>/\..*\.${ext}$`,
+    // config files in the root dir
+    String.raw`<rootDir>/.*\.config\.${ext}$`,
+    String.raw`<rootDir>/.*rc\.${ext}$`,
+    // types
+    String.raw`/interfaces/`,
+    String.raw`/types/`,
   ],
 };
