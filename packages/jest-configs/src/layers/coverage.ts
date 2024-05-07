305bbd3e1457212ebbd8215ae5a2bf6c37a00969
Louis Orleans <louis@orleans.io>
2024-05-06 18:30:47 -0700

    ♻️🐛 ignore coverage for more files

--- packages/jest-configs/src/layers/coverage.ts
+++ packages/jest-configs/src/layers/coverage.ts
@@ -1,25 +1,53 @@
 import { Config } from 'jest';
 
 const ext = String.raw`[cm]?[jt]sx?`;
+const ext_glob = String.raw`?(c|m)(j|t)s?(x)`;
 
 export const config: Config = {
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.?(c|m)(t|j)s?(x)'],
   coverageDirectory: './coverage',
+  collectCoverageFrom: [`**/*.${ext_glob}`],
   coveragePathIgnorePatterns: [
-    // dependencies
-    String.raw`/node_modules/`,
-    // migration files
-    String.raw`/migrations/`,
-    // Mikro ORM config
-    String.raw`/mikro-orm\.config\.ts$`,
-    // hidden files in the root dir (usually configs)
-    String.raw`<rootDir>/\..*\.${ext}$`,
-    // config files in the root dir
-    String.raw`<rootDir>/.*\.config\.${ext}$`,
-    String.raw`<rootDir>/.*rc\.${ext}$`,
-    // types
-    String.raw`/interfaces/`,
-    String.raw`/types/`,
-  ],
+    [
+      // dependencies
+      String.raw`/node_modules/`,
+    ],
+    [
+      // migration files
+      String.raw`/migrations/`,
+    ],
+    [
+      // Mikro ORM config
+      String.raw`/mikro-orm\.config\.ts$`,
+    ],
+    [
+      // hidden files in the root dir (usually configs)
+      String.raw`<rootDir>/\..*\.${ext}$`,
+    ],
+    [
+      // config files in the root dir
+      String.raw`<rootDir>/.*\.config\.${ext}$`,
+      String.raw`<rootDir>/.*rc\.${ext}$`,
+    ],
+    [
+      // types
+      String.raw`/interfaces/`,
+      String.raw`/types/`,
+      String.raw`\.d.ts$`,
+    ],
+    [
+      // outputs
+      String.raw`/dist/`,
+      String.raw`/out/`,
+    ],
+    [
+      // tests
+      String.raw`/test/`,
+      String.raw`/__tests__/`,
+    ],
+    [
+      // coverage
+      String.raw`/coverage/`,
+    ],
+  ].flat(),
 };
