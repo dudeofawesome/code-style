ea433fad08275794999f6facb4ceb26aa2661c28
Louis Orleans <louis@orleans.io>
2024-05-08 14:45:54 -0700

    ✅ fix jest config link

--- packages/eslint-plugin-nest/jest.config.mjs
+++ packages/eslint-plugin-nest/jest.config.mjs
@@ -1,4 +1,4 @@
-import { config } from '@code-style/jest-configs/ts-esm';
+import { config } from '@code-style/jest-configs/ts/esbuild/esm';
 
 // eslint-disable-next-line import/no-default-export
 export default config;
