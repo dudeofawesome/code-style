b0f7c3fb35e56c058769a1b2bd97ecc602ec06fd
Louis Orleans <louis@orleans.io>
2024-05-08 23:10:13 -0700

    🚨 fix lint errors

--- packages/create-configs/bin/create-configs.js
+++ packages/create-configs/bin/create-configs.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S node --trace-uncaught
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
 import { main } from '../dist/index.js';
 
-main();
+void main();
