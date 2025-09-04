89429f5a6be73bc787536208ff69de377a8abd44
Louis Orleans <louis@orleans.io>
2025-09-04 12:17:55 -0700

    🐛 load prettier plugins from code-style package path

--- packages/code-style/src/prettier-plugin-packagejson.d.ts
+++ packages/code-style/src/prettier-plugin-packagejson.d.ts
@@ -1,7 +1,5 @@
 declare module 'prettier-plugin-packagejson' {
   import type { Plugin } from 'prettier';
 
-  const plugin: Plugin;
-
-  export default plugin;
+  export const { parsers, options }: Plugin;
 }
